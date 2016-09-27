e1ee9f421ad56bffbf6156bdff606e18eeebe400
yodivdhende <yodi.vandenhende@student.howest.be>
2016-09-27 02:23:18 +0200

    visual class aangemaakt

---
+++ src/Book/ClassBook.java
@@ -0,0 +1,20 @@
+package Book;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marisela Poot
+ */
+public class ClassBook {
+    
+    private Page[] Pages;
+    
+    public ClassBook() {
+    Pages = new Page [5];
+    
+    }
+     
+}
