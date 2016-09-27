cbbb7d0c0c76761138de068d53e625ab71ad46a6
Sander <sander.vandeburie@gmail.com>
2016-09-27 09:28:35 +0200

    Update

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -25,18 +25,17 @@ public final class Game extends Application{
     Item currentHolding;
     Stage stage;
     Item tmp;
+    Group root;
+    Scene theScene;
+    Canvas canvas;
+    GraphicsContext gc;
+    Image img;
 
-    public Game(Stage stage) {
+    public Game(Stage stage) throws Exception {
         this.stage = stage;
 
         Init();
         LoadGame();
-
-
-
-
-        //GetCurrentRoom().GetCurrentView().addMouseListener(this);
-        //addMouseListener(this);
     }
 
     private void Init(){
@@ -58,34 +57,30 @@ public final class Game extends Application{
 
         View view1 = new View("file:resources\\WallRoom1.jpg",1);
 
-        Item item1v1 = new Item(1,880,500,"file:resources\\Vault.png",200,200);
-        Item item1v2 = new Item(1,100, 100,"file:resources\\Painting.jpg",150,300);
+        Item item1v1 = new Item(1,880,100,"file:resources\\Door-closed.png",250,600);
         view1.addItem(item1v1);
-        view1.addItem(item1v2);
         views[0] = view1;
 
         View view2 = new View("file:resources\\WallRoom1.jpg",2);
 
-        //Item item2v1 = new Item(1,1,1,"Chair");
-        //Item item2v2 = new Item(1,1,1,"Thrashcan");
-        //view1.addItem(item2v1);
-        //view1.addItem(item2v2);
+        Item item2v1 = new Item(2,100,400,"file:resources\\Desk.png",500,300);
+        Item item2v2 = new Item(2,100,600,"file:resources\\Trash-with-page.png",100,50);
+        view2.addItem(item2v1);
+        view2.addItem(item2v2);
         views[1] = view2;
 
         View view3 = new View("file:resources\\WallRoom1.jpg",3);
 
-        //Item item3v1 = new Item(1,1,1,"Vault");
-        //view1.addItem(item3v1);
+        Item item3v1 = new Item(3,300,100,"file:resources\\Window.jpg",300,500);
+        view3.addItem(item3v1);
         views[2] = view3;
 
         View view4 = new View("file:resources\\WallRoom1.jpg",4);
 
-        //Item item4v1 = new Item(1,1,1,"Coathanger");
-        //Item item4v2 = new Item(1,1,1,"Door");
-        //Item item4v3 = new Item(1,1,1,"Plant");
-        //view1.addItem(item4v1);
-        //view1.addItem(item4v2);
-        //view1.addItem(item4v3);
+        Item item4v1 = new Item(4,880,500,"file:resources\\Vault.png",200,200);
+        Item item4v2 = new Item(5,70, 200,"file:resources\\RomanPoster.png",300,200);
+        view4.addItem(item4v1);
+        view4.addItem(item4v2);
         views[3] = view4;
 
         room.setViews(views);
@@ -104,33 +99,35 @@ public final class Game extends Application{
 
     }
 
-    private void LoadGame(){
-
+    private void LoadGame() throws Exception {
         currentRoom = 1;
         GetCurrentRoom().setCurrentView(1);
-
-        Draw(stage);
+        start(stage);
     }
+    @Override
+    public void start(Stage theStage) throws Exception {
+        root = null;
+        theScene = null;
+        canvas = null;
+        img = null;
+        gc = null;
 
-    private void Draw(Stage theStage)
-    {
-        Group root = new Group();
-        Scene theScene = new Scene( root );
-        Canvas canvas = new Canvas( 1280, 800);
+        root = new Group();
+        theScene = new Scene( root );
+        canvas = new Canvas( 1280, 800);
 
         theStage.setTitle( "Dreadle" );
         theStage.setScene( theScene );
 
         root.getChildren().add( canvas );
 
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc = canvas.getGraphicsContext2D();
 
         theScene.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
             @Override
             public void handle(javafx.scene.input.MouseEvent event) {
                 tmp = GetCurrentRoom().GetCurrentView().findItem((int) event.getX(),(int) event.getY());
-                CheckNext((int) event.getX(),(int) event.getY());
-                CheckPrevious((int) event.getX(),(int) event.getY());
+                ViewChange((int) event.getX(),(int) event.getY());
                 if(tmp != null)
                 {
                     tmp.interactWith(currentHolding);
@@ -138,7 +135,12 @@ public final class Game extends Application{
             }
         });
 
-        Image img = new Image(GetCurrentRoom().GetCurrentView().getBackground(),1180, 800, false, false);
+        Paint();
+
+        theStage.show();
+    }
+    private void Paint(){
+        img = new Image(GetCurrentRoom().GetCurrentView().getBackground(),1180, 800, false, false);
         gc.drawImage( img, 0, 0 );
 
         for (Item item : GetCurrentRoom().GetCurrentView().getItems())
@@ -151,50 +153,43 @@ public final class Game extends Application{
         gc.drawImage(img, 20, 650);
         img = new Image("file:resources\\Next.png",150,100,false,false);
         gc.drawImage(img, 1000, 650);
-
-        theStage.show();
     }
 
     private Room GetCurrentRoom(){
         return rooms[currentRoom-1];    }
 
-
-    private void CheckPrevious(int x , int y)
-    {
+    private void ViewChange(int x, int y){
+        CheckNext(x,y);
+        CheckPrevious(x,y);
+    }
+    private void CheckPrevious(int x , int y){
         if (y > 650 && y < 750 && x < 120 && x > 20)
         {
             GoPrevious();
         }
     }
-    private void CheckNext(int x , int y)
-    {
+    private void CheckNext(int x , int y){
         if (y > 650 && y < 750 && x < 1100 && x > 1000)
         {
             GoNext();
         }
     }
-    private void GoPrevious()
-    {
+    private void GoPrevious(){
         if(GetCurrentRoom().GetCurrentView().getId() == 1)
             GetCurrentRoom().setCurrentView(4);
         else
-            GetCurrentRoom().setCurrentView(currentRoom--);
+            GetCurrentRoom().setCurrentView(GetCurrentRoom().GetCurrentView().getId()-1);
 
-        Draw(stage);
-        System.out.println("Previous");
+        Paint();
     }
-    private void GoNext()
-    {
+    private void GoNext(){
         if(GetCurrentRoom().GetCurrentView().getId() == 4)
             GetCurrentRoom().setCurrentView(1);
         else
-            GetCurrentRoom().setCurrentView(currentRoom++);
+            GetCurrentRoom().setCurrentView(GetCurrentRoom().GetCurrentView().getId()+1);
 
-        Draw(stage);
-        System.out.println("Next");
+        Paint();
     }
 
-    @Override
-    public void start(Stage theStage) throws Exception {
-          }
+
 }
