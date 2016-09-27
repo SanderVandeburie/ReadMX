ba584bcf2f8d1f31f728b42183c4106e43a098a3
Sander <sander.vandeburie@gmail.com>
2016-09-27 16:22:11 +0200

    Update

--- src/Book/Book.java
+++ src/Book/Book.java
@@ -1,24 +1,95 @@
-package Book;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package Book;
 
-/**
- *
- * @author Marisela Poot
- */
-import javafx.scene.image.Image;
+
+import java.awt.Graphics;
+import javax.swing.*;
+
+
 public class Book {
 
-    private Page[] Pages;
-    private Image coverImage;
+    private int x, y;
+    private int place;
+    private String[] page;
 
-    public Book(String coverImagePath) {
-        Pages = new Page [5];
-        coverImage =  new Image(coverImagePath);
+
+    public Book (int xpos, int ypos){
+
+        page = new String[7];
+        place = 0;
+        x = xpos;
+        y = ypos;
+    }
+
+    public Book (){
+
+        //page = new  [7];
+        place = 0;
+        //page1 = new ImageIcon (getClass().getResource ("1.jpg"));
+
+        //x = page.getIconWidth;
+       // y = page.getIconHeight;
+
+    }
+
+    public void InsertPage (String Path){
+
+        for (int i=0; i<7; i++){
+
+            if (page[i] == null){
+
+                page[i] = Path;
+
+                i =7;
+            }
+
+        }
+
+    }
+
+    public void Next (){
+
+        place++;
+
+        if (place == 7){
+
+            place = 0;
+        }
+    }
+    public void Previous (){
+
+        place--;
+
+        if (place == 7){
+
+            place = 0;
+        }
+    }
+
+   // public ImageIcon getPage (){
+
+        //return page[place];
+
+   // }
+
+
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 
 
-}
 
+
+}
